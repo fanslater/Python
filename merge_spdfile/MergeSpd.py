d76884053c22bc7c40997f54ef104a5d8e712b81
fanslater <664604145@qq.com>
2019-09-17 10:30:10 +0800

    1.备份spd文件时，加上系统时间 2.现在工具可以重复执行任务，不需要重启 3.现在工具会自动记住上一次使用的全量SPD文件路径。 1.全量SPD的默认路径设置为【./bin/KCBPSPD.xml】

--- merge_spdfile/MergeSpd.py
+++ merge_spdfile/MergeSpd.py
@@ -4,6 +4,7 @@ import os
 import sys
 import CommentedTreeBuilder as ctb
 import shutil
+import datetime as dt
 
 try:
     import xml.etree.cElementTree as et
@@ -19,62 +20,66 @@ def formatXml(element, indent, newline, level=0):
             subelement.tail = newline + indent * level
         formatXml(subelement, indent, newline, level=level + 1)
 
-full_spd = raw_input("输入全量SPD路径（不输默认【./KCBPSPD.xml】）\n")
-if len(full_spd) == 0 or full_spd.isspace():
-    full_spd = "./KCBPSPD.xml"
+full_spd_default = "./bin/KCBPSPD.xml"
+while 1:
+    full_spd = raw_input("请输入全量kcbpspd.xml路径（可将文件拖拽到当前窗口）（不输默认【%s】）\n" % full_spd_default)
+    if len(full_spd) == 0 or full_spd.isspace():
+        full_spd = full_spd_default
+    else:
+        full_spd_default = full_spd
 
-add_spd = raw_input("输入增量SPD路径（不输默认【./add_KCBPSPD.xml】）\n")
-if len(add_spd) == 0 or add_spd.isspace():
-    add_spd = "./add_KCBPSPD.xml"
+    add_spd = raw_input("请输入增量kcbpspd.xml路径（可将文件拖拽到当前窗口）（不输默认【./add_KCBPSPD.xml】）\n")
+    if len(add_spd) == 0 or add_spd.isspace():
+        add_spd = "./add_KCBPSPD.xml"
 
-out_spd = full_spd
+    out_spd = full_spd
 
-# 备份原来的全量SPD文件
-try:
-    shutil.copy(full_spd, os.path.splitext(full_spd)[0] + "_bak.xml")
-except Exception as ex:
-    print("backup old full_spd error!", ex)
+    # 备份原来的全量SPD文件
+    try:
+        shutil.copy(full_spd, os.path.splitext(full_spd)[0] + "." + dt.datetime.now().strftime("%Y%m%d%H%M%S") + ".backup.xml")
+    except Exception as ex:
+        print("backup old full_spd error!", ex)
+        os.system("pause")
+        sys.exit(-1)
+
+    # 读取全量SPD
+    try:
+        treeFullSpd = et.parse(full_spd, parser=ctb.CommentedTreeBuilder())
+        ndFullSpdRoot = treeFullSpd.getroot()
+    except Exception as ex:
+        print("parse xml [%s] error!" % full_spd, ex)
+        os.system("pause")
+        sys.exit(-1)
+    else:
+        print("load xml[%s] success!" % full_spd)
+
+    # 读取增量SPD
+    try:
+        treeAddSpd = et.parse(add_spd, parser=ctb.CommentedTreeBuilder())
+        ndAddSpdRoot = treeAddSpd.getroot()
+    except Exception as ex:
+        print("parse xml [%s] error!" % add_spd, ex)
+        os.system("pause")
+        sys.exit(-1)
+    else:
+        print("load xml[%s] success!" % add_spd)
+
+    # 将增量spd的内容追加到全量spd
+    for child in ndAddSpdRoot:
+        ndFullSpdRoot.append(child)
+
+    # 去重
+    lstProgram = ndFullSpdRoot.findall("program")
+    for i in range(len(lstProgram) - 1, 0, -1):
+        for j in range(i - 1, -1, -1):
+            if i != j and lstProgram[i].attrib["name"] == lstProgram[j].attrib["name"]:
+                try:
+                    ndFullSpdRoot.remove(lstProgram[j])  # 这里操作list是没用的，list只是缓存了节点对象地址。要去root节点对接里面删除节点。
+                except:
+                    pass
+
+    # 重新美化xml
+    formatXml(ndFullSpdRoot, "  ", "\n")
+    treeFullSpd.write(out_spd, "UTF-8")
+    print("处理成功，输出文件=[%s]\n\n" % out_spd)
     os.system("pause")
-    sys.exit(-1)
-
-# 读取全量SPD
-try:
-    treeFullSpd = et.parse(full_spd, parser=ctb.CommentedTreeBuilder())
-    ndFullSpdRoot = treeFullSpd.getroot()
-except Exception as ex:
-    print("parse xml [%s] error!" % full_spd, ex)
-    os.system("pause")
-    sys.exit(-1)
-else:
-    print("load xml[%s] success!" % full_spd)
-
-# 读取增量SPD
-try:
-    treeAddSpd = et.parse(add_spd, parser=ctb.CommentedTreeBuilder())
-    ndAddSpdRoot = treeAddSpd.getroot()
-except Exception as ex:
-    print("parse xml [%s] error!" % add_spd, ex)
-    os.system("pause")
-    sys.exit(-1)
-else:
-    print("load xml[%s] success!" % add_spd)
-
-# 将增量spd的内容追加到全量spd
-for child in ndAddSpdRoot:
-    ndFullSpdRoot.append(child)
-
-# 去重
-lstProgram = ndFullSpdRoot.findall("program")
-for i in range(len(lstProgram) - 1, 0, -1):
-    for j in range(i - 1, -1, -1):
-        if i != j and lstProgram[i].attrib["name"] == lstProgram[j].attrib["name"]:
-            try:
-                ndFullSpdRoot.remove(lstProgram[j])  # 这里操作list是没用的，list只是缓存了节点对象地址。要去root节点对接里面删除节点。
-            except:
-                pass
-
-# 重新美化xml
-formatXml(ndFullSpdRoot, "  ", "\n")
-treeFullSpd.write(out_spd, "UTF-8")
-print("处理成功，输出文件=[%s]" % out_spd)
-os.system("pause")
